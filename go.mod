056fc93f945a1bf4bcf368e8b3a0ea1ab5ac41f4
yumekiti <yumekiti120440@gmail.com>
2022-08-17 01:49:05 +0900

    `add` env

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/labstack/echo/v4 v4.8.0 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 
+	"github.com/yumekiti/eccSchoolApp-api/config"
 	"github.com/yumekiti/eccSchoolApp-api/infrastructure"
 	"github.com/yumekiti/eccSchoolApp-api/interface/handler"
 	"github.com/yumekiti/eccSchoolApp-api/usecase"
 )
+
+func init() {
+	config.LoadEnv()
+}
 
 func main() {
 	newsRepository := infrastructure.NewNewsRepository()
