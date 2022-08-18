c2c31edb579ed305ff38b995361976b4bbf5f5c1
yumekiti <yumekiti120440@gmail.com>
2022-08-19 04:52:48 +0900

    `add` news-only usecase

--- usecase/news-only_usecase.go
+++ usecase/news-only_usecase.go
@@ -1,1 +1,27 @@
 package usecase
+
+import (
+	"github.com/yumekiti/eccSchoolApp-api/domain"
+	"github.com/yumekiti/eccSchoolApp-api/domain/repository"
+)
+
+type NewsOnlyUsecase interface {
+	Get(*domain.User) (*domain.NewsOnly, error)
+}
+
+type newsOnlyUsecase struct {
+	newsOnlyRepository repository.NewsOnlyRepository
+}
+
+func NewNewsOnlyUsecase(newsOnlyRepository repository.NewsOnlyRepository) NewsOnlyUsecase {
+	return &newsOnlyUsecase{newsOnlyRepository: newsOnlyRepository}
+}
+
+func (u *newsOnlyUsecase) Get(user *domain.User) (*domain.NewsOnly, error) {
+	getNewsOnly, err := u.newsOnlyRepository.Get(user)
+	if err != nil {
+		return nil, err
+	}
+
+	return getNewsOnly, nil
+}
