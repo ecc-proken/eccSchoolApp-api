b8c94130c3d091f67414d32204d188f086a7915c
yumekiti <yumekiti120440@gmail.com>
2022-08-19 06:49:10 +0900

    `add` news-only

--- infrastructure/news-only_repository.go
+++ infrastructure/news-only_repository.go
@@ -1,8 +1,11 @@
 package infrastructure
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
+	"github.com/gocolly/colly"
 	"github.com/yumekiti/eccSchoolApp-api/config"
 	"github.com/yumekiti/eccSchoolApp-api/domain"
 	"github.com/yumekiti/eccSchoolApp-api/domain/repository"
@@ -17,9 +20,58 @@ func NewNewsOnlyRepository() repository.NewsOnlyRepository {
 func (r *NewsOnlyRepository) Get(id string, user *domain.User) (*domain.NewsOnly, error) {
 	c := config.ECCLogin(user)
 
-	c.Visit(os.Getenv("APP_DOMAIN") + os.Getenv("APP_NEWS"))
+	// 初期化
+	title := ""
+	body := ""
+	date := ""
+	tag := ""
+	attachment := []string{}
+
+	// ニュースを取得し、それぞれ格納
+	// title
+	c.OnHTML(".title", func(e *colly.HTMLElement) {
+		title = e.Text
+	})
+	// body
+	c.OnHTML(".news div ~ div", func(e *colly.HTMLElement) {
+		// htmlを取得
+		html, err := e.DOM.Html()
+		if err != nil {
+			return
+		}
+		// bodyにhtmlを格納されるまで待機
+		for body == "" {
+			body = html
+		}
+		body = strings.Replace(body, "\n", "", -1)
+	})
+	// date
+	c.OnHTML(".detail_title01 div", func(e *colly.HTMLElement) {
+		date = strings.Split(e.Text, " ")[0]
+	})
+	// tag
+	c.OnHTML(".icon01", func(e *colly.HTMLElement) {
+		tag = e.Text
+	})
+	// attachment
+	c.OnHTML(".main article p", func(e *colly.HTMLElement) {
+		e.ForEach("a", func(_ int, e *colly.HTMLElement) {
+			if e.Attr("href") == "../" {
+				return
+			}
+			attachment = append(attachment, e.Attr("href"))
+		})
+	})
+
+	c.Visit(os.Getenv("APP_DOMAIN") + os.Getenv("APP_NEWS") + os.Getenv("APP_NEWS_ONLY_FRONT") + id + os.Getenv("APP_NEWS_ONLY_BACK"))
+
+	fmt.Print(body)
 
 	return &domain.NewsOnly{
-		Title: id,
+		Title:      title,
+		Body:       body,
+		Date:       date,
+		Tag:        tag,
+		Attachment: attachment,
 	}, nil
 }
