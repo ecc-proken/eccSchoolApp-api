25d888e552ebefffc89114f264a51c1707655243
yumekiti <yumekiti120440@gmail.com>
2022-08-20 07:31:34 +0900

    `feat` Attendance

--- config/scraping.go
+++ config/scraping.go
@@ -2,7 +2,9 @@ package config
 
 import (
 	"log"
+	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gocolly/colly"
 	"github.com/yumekiti/eccSchoolApp-api/domain"
@@ -27,52 +29,49 @@ func ECCLogin(user *domain.User) *colly.Collector {
 	return c
 }
 
-// func FalconLogin(user *domain.User) (*colly.Collector, string) {
-// 	// リダイレクト先を取得
-// 	target_url := "https://falcon.ecc.ac.jp/eccmo/mo0100/mo0100_01.aspx"
-// 	req, _ := http.NewRequest("HEAD", target_url, nil)
-// 	resp, _ := http.DefaultTransport.RoundTrip(req)
-// 	defer resp.Body.Close()
-// 	location := resp.Header["Location"][0]
+func FalconLogin(user *domain.User) (*colly.Collector, string) {
+	// リダイレクト先を取得
+	target_url := os.Getenv("FALCON") + "/eccmo/mo0100/mo0100_01.aspx"
+	req, _ := http.NewRequest("HEAD", target_url, nil)
+	resp, _ := http.DefaultTransport.RoundTrip(req)
+	defer resp.Body.Close()
+	location := resp.Header["Location"][0]
 
-// 	// token を取得
-// 	token := strings.Split(strings.Split(location, "(")[2], ")")[0]
+	// token を取得
+	token := strings.Split(strings.Split(location, "(")[2], ")")[0]
 
-// 	c := colly.NewCollector()
+	c := colly.NewCollector()
 
-// 	// 初期化
-// 	action := ""
-// 	viewstate := ""
+	// 初期化
+	action := ""
+	viewstate := ""
 
-// 	// formのactionを取得
-// 	c.OnHTML("form", func(e *colly.HTMLElement) {
-// 		action = e.Attr("action")
-// 	})
+	// formのactionを取得
+	c.OnHTML("form", func(e *colly.HTMLElement) {
+		action = e.Attr("action")
+	})
 
-// 	// inputのvalueを取得
-// 	c.OnHTML("input", func(e *colly.HTMLElement) {
-// 		if e.Attr("name") == "__VIEWSTATE" {
-// 			viewstate = e.Attr("value")
-// 		}
-// 	})
+	// inputのvalueを取得
+	c.OnHTML("input", func(e *colly.HTMLElement) {
+		if e.Attr("name") == "__VIEWSTATE" {
+			viewstate = e.Attr("value")
+		}
+	})
 
-// 	c.Visit("https://falcon.ecc.ac.jp/eccmo/(S(" + token + "))/MO0100/MO0100_01.aspx")
+	c.Visit(os.Getenv("FALCON") + "/eccmo/(S(" + token + "))/MO0100/MO0100_01.aspx")
 
-// 	err := c.Post("https://falcon.ecc.ac.jp/eccmo/(S("+token+"))/MO0100/"+action,
-// 		map[string]string{
-// 			"__VIEWSTATE":     viewstate,
-// 			"__EVENTTARGET":   "",
-// 			"__EVENTARGUMENT": "",
-// 			"txtUserId":       user.Id,
-// 			"txtPassword":     user.Password,
-// 		})
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
+	err := c.Post(os.Getenv("FALCON")+"/eccmo/(S("+token+"))/MO0100/"+action,
+		map[string]string{
+			"__VIEWSTATE":     viewstate,
+			"__EVENTTARGET":   "",
+			"__EVENTARGUMENT": "",
+			"txtUserId":       user.Id,
+			"txtPassword":     user.Password,
+			"btnLogin":        "",
+		})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-// 	c.OnResponse(func(r *colly.Response) {
-// 		r.Request.Headers.Set("Referer", "https://falcon.ecc.ac.jp/eccmo/(S("+token+"))/MO0100/MO0100_01.aspx")
-// 	})
-
-// 	return c, token
-// }
+	return c, token
+}
--- infrastructure/attendance_repository.go
+++ infrastructure/attendance_repository.go
@@ -1,6 +1,13 @@
 package infrastructure
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/gocolly/colly"
+	"github.com/yumekiti/eccSchoolApp-api/config"
 	"github.com/yumekiti/eccSchoolApp-api/domain"
 	"github.com/yumekiti/eccSchoolApp-api/domain/repository"
 )
@@ -12,5 +19,80 @@ func NewAttendanceRepository() repository.AttendanceRepository {
 }
 
 func (r *AttendanceRepository) Get(user *domain.User) ([]*domain.Attendance, error) {
-	return []*domain.Attendance{}, nil
+	c, token := config.FalconLogin(user)
+
+	// 初期化
+	title := []string{}
+	rate := []string{}
+	absence := []string{}
+	lateness := []string{}
+
+	tmp := [][]string{}
+	action := ""
+	viewstate := ""
+	venttarget := "lstSyussekiRitsu"
+
+	c.OnHTML("input", func(e *colly.HTMLElement) {
+		if e.Attr("name") == "__VIEWSTATE" {
+			viewstate = e.Attr("value")
+		}
+	})
+
+	c.OnHTML("form", func(e *colly.HTMLElement) {
+		action = e.Attr("action")
+	})
+
+	c.OnHTML("form", func(e *colly.HTMLElement) {
+		e.ForEach("a", func(_ int, e *colly.HTMLElement) {
+			if e.Text == "戻る" {
+				return
+			}
+			if len(strings.Split(e.Text, " ")) == 2 {
+				tmp = append(tmp, strings.Split(e.Text, " "))
+			}
+		})
+	})
+
+	c.Visit(os.Getenv("FALCON") + "/eccmo/(S(" + token + "))/MO0500/MO0500_01.aspx?mode=1")
+
+	for i, v := range tmp {
+		// title
+		title = append(title, v[0])
+		// rate
+		rate = append(rate, v[1])
+
+		flag := false
+		c.OnHTML("form", func(e *colly.HTMLElement) {
+			if flag {
+				return
+			}
+
+			absence = append(absence, strings.Split(e.Text, "欠　席:")[1][:1])
+			lateness = append(lateness, strings.Split(e.Text, "遅　刻:")[1][:1])
+
+			flag = true
+		})
+
+		err := c.Post(os.Getenv("FALCON")+"/eccmo/(S("+token+"))/MO0500/"+action,
+			map[string]string{
+				"__VIEWSTATE":     viewstate,
+				"__EVENTTARGET":   venttarget,
+				"__EVENTARGUMENT": fmt.Sprint(i),
+			})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	attendance := []*domain.Attendance{}
+	for i, v := range title {
+		attendance = append(attendance, &domain.Attendance{
+			Title:    v,
+			Rate:     rate[i],
+			Absence:  absence[i],
+			Lateness: lateness[i],
+		})
+	}
+
+	return attendance, nil
 }
