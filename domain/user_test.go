a3fbcca190f2bb9e87d3169a2f0a37294a623ce6
yumekiti <yumekiti120440@gmail.com>
2022-11-21 11:45:29 +0900

    `refactoring`

--- domain/user_test.go
+++ domain/user_test.go
@@ -8,12 +8,12 @@ import (
 
 func TestUser(t *testing.T) {
 	user := User{
-		ID:       "1",
-		Password: "2200000",
+		ID:       "2200000",
+		Password: "hoge",
 		UUID:		 "00000000-0000-0000-0000-000000000000",
 	}
 	
-	assert.Equal(t, "1", user.ID)
-	assert.Equal(t, "2200000", user.Password)
+	assert.Equal(t, "2200000", user.ID)
+	assert.Equal(t, "hoge", user.Password)
 	assert.Equal(t, "00000000-0000-0000-0000-000000000000", user.UUID)
 }
